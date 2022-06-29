a8cd7c6f7e9785224fa4e515616536820fc5a368
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2022-06-29 22:50:42 +0300

    init airpol-service

---
+++ airpollutionservice/src/main/java/com/mobileactionbootcamp/airpollutionservice/AirpollutionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mobileactionbootcamp.airpollutionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirpollutionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirpollutionserviceApplication.class, args);
+	}
+
+}
---
+++ airpollutionservice/src/test/java/com/mobileactionbootcamp/airpollutionservice/AirpollutionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileactionbootcamp.airpollutionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirpollutionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
