3209589715f06da8d91dbafcbf13c2c52d844a57
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2022-06-30 19:20:06 +0300

    caqi-service init

---
+++ caqi-classification-service/src/test/java/com/mobileactionbootcamp/caqiclassificationservice/CaqiClassificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileactionbootcamp.caqiclassificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaqiClassificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
