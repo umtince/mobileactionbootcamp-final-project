d6bd469c65db99257227226d3148fc6b22fc8679
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2022-07-03 01:51:51 +0300

    db crud completed. log missing

--- air-quality-service/src/main/java/com/mobileactionbootcamp/airqualityservice/aqs/service/AqsAirQualityService.java
+++ air-quality-service/src/main/java/com/mobileactionbootcamp/airqualityservice/aqs/service/AqsAirQualityService.java
@@ -3,8 +3,6 @@ package com.mobileactionbootcamp.airqualityservice.aqs.service;
 import com.mobileactionbootcamp.airqualityservice.aqs.dao.AqsAirQualityDocumentDao;
 import com.mobileactionbootcamp.airqualityservice.aqs.document.AqsAirQualityDocument;
 import com.mobileactionbootcamp.airqualityservice.aqs.document.AqsResults;
-import com.mobileactionbootcamp.airqualityservice.cls.model.ClsBaseComponents;
-import com.mobileactionbootcamp.airqualityservice.cls.model.ClsCategories;
 import com.mobileactionbootcamp.airqualityservice.cls.service.ClsClassificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,108 +28,44 @@ public class AqsAirQualityService {
             throw new Exception("End date can not be before start date!");
         }
 
-        /*AqsAirQualityDocument aqsAirQualityDocument = new AqsAirQualityDocument();
-        aqsAirQualityDocument.setCity(city);
-
-        AqsResults aqsResults = new AqsResults();
-        aqsResults.setDate(parseDateToString(start));
-
-        ClsCategories tempCategories = new ClsCategories();
-        tempCategories.setCo(new ClsBaseComponents("Good"));
-        tempCategories.setO3(new ClsBaseComponents("Good"));
-        tempCategories.setSo2(new ClsBaseComponents("Good"));
-
-        aqsResults.addCategory(tempCategories);
-
-        //aqsAirQualityDocument.addResuls(aqsResults);
-
-        List<AqsAirQualityDocument> aqsAirQualityDocumentList = aqsAirQualityDocumentDao.findAll();
-
-        for(AqsAirQualityDocument document : aqsAirQualityDocumentList){
-            if(document.getCity().equals(aqsAirQualityDocument.getCity())){
-
-                document.getResults().add(aqsResults);
-                document = aqsAirQualityDocumentDao.save(document);
-                return document;
-            }
-        }
-        aqsAirQualityDocument.addResuls(aqsResults);
-        aqsAirQualityDocumentDao.save(aqsAirQualityDocument);*/
-
         AqsAirQualityDocument aqsAirQualityDocument = getAirQualityDocumentWithResults(city, start, end);
-        //aqsAirQualityDocumentDao.save(aqsAirQualityDocument);
 
         return aqsAirQualityDocument;
     }
 
     private AqsAirQualityDocument getAirQualityDocumentWithResults(String city, LocalDate start, LocalDate end){
 
-        //DB request is made at this point to prevent repeating DB access (performance optimization).
-        List<AqsAirQualityDocument> aqsAirQualityDocumentList = aqsAirQualityDocumentDao.findAll();
-        LocalDate incremenentedStart = start;
-        AqsResults existingResultByDate;
-        AqsAirQualityDocument responseDocument = createEmptyDocument();
-        AqsAirQualityDocument documentInDb = getExistingDocumentByCity(city, aqsAirQualityDocumentList);
+        AqsAirQualityDocument responseDocument = new AqsAirQualityDocument();
+        responseDocument.setCity(city);
 
+        AqsAirQualityDocument documentInDb = getExistingDocumentByCity(city);
         if(documentInDb == null){
-            documentInDb = createEmptyDocument();
+            documentInDb = new AqsAirQualityDocument();
+            documentInDb.setCity(city);
         }
 
-        responseDocument.setCity(city);
-        //responseDocument.setId(getExistingDocumentIdByCity(city, aqsAirQualityDocumentList));
+        LocalDate incrementedStart = start;
+        while(incrementedStart.isBefore(end.plusDays(1))){
 
-        while (incremenentedStart.isBefore(end)){
-
-            existingResultByDate = getExistingAirQualityResultsByDate(city, incremenentedStart, aqsAirQualityDocumentList);
-
-            if (existingResultByDate != null) {
-                //returned from database
-                responseDocument.addResuls(existingResultByDate);
-
-                /**
-                 * TODO: log returned from db
-                 * */
-            }else {
-                // returned from api
-                /*AqsResults resultsFromApi = new AqsResults();
-                resultsFromApi.setDate(parseDateToString(incremenentedStart));
-                resultsFromApi.addCategory(clsClassificationService.getAqiClassification(city, parseDateToString(incremenentedStart), parseDateToString(incremenentedStart.plusDays(1))));*/
-
-                AqsResults aqsResults;
-                aqsResults = getResultsFromApi(city, incremenentedStart, incremenentedStart.plusDays(1));
-                responseDocument.addResuls(aqsResults);
-                documentInDb.addResuls(aqsResults);
+            AqsResults existingResultsOfCity = getExistingResultsOfCityByDate(incrementedStart, documentInDb);
+            if(existingResultsOfCity != null){
+                //databasede var
+                responseDocument.addResuls(existingResultsOfCity);
+            } else {
+                //api dan al!
+                AqsResults resultsFromApi = getAirQualityResultsFromApi(city, incrementedStart, incrementedStart.plusDays(1));
+                responseDocument.addResuls(resultsFromApi);
+                documentInDb.addResuls(resultsFromApi);
                 documentInDb = aqsAirQualityDocumentDao.save(documentInDb);
-                /**
-                 * TODO: log returned from api
-                 * */
             }
 
-            incremenentedStart = incremenentedStart.plusDays(1);
+            incrementedStart = incrementedStart.plusDays(1);
         }
 
         return responseDocument;
     }
 
-    private AqsAirQualityDocument getExistingDocumentByCity(String city, List<AqsAirQualityDocument> aqsAirQualityDocumentList){
-        for(AqsAirQualityDocument document : aqsAirQualityDocumentList){
-            if(document.getCity().equals(city)){
-                return document;
-            }
-        }
-        return null;
-    }
-
-    private String getExistingDocumentIdByCity(String city, List<AqsAirQualityDocument> aqsAirQualityDocumentList){
-        for(AqsAirQualityDocument document : aqsAirQualityDocumentList){
-            if(document.getCity().equals(city)){
-                return document.getId();
-            }
-        }
-        return null;
-    }
-
-    private AqsResults getResultsFromApi(String city, LocalDate start, LocalDate end){
+    private AqsResults getAirQualityResultsFromApi(String city, LocalDate start, LocalDate end){
         AqsResults resultsFromApi = new AqsResults();
         resultsFromApi.setDate(parseDateToString(start));
         resultsFromApi.addCategory(clsClassificationService.getAqiClassification(city, parseDateToString(start), parseDateToString(end)));
@@ -139,28 +73,24 @@ public class AqsAirQualityService {
         return resultsFromApi;
     }
 
-    private AqsAirQualityDocument createEmptyDocument(){
+    private AqsResults getExistingResultsOfCityByDate(LocalDate date, AqsAirQualityDocument documentInDb){
+        String s_date = parseDateToString(date);
 
-       /* ClsCategories clsCategories = new ClsCategories();
+        for(AqsResults results : documentInDb.getResults()){
+            if(results.getDate().equals(s_date)){
+                return results;
+            }
+        }
 
-        AqsResults aqsResults = new AqsResults();
-        aqsResults.addCategory(clsCategories);*/
-
-        AqsAirQualityDocument aqsAirQualityDocument = new AqsAirQualityDocument();
-        //aqsAirQualityDocument.addResuls(aqsResults);
-
-        return aqsAirQualityDocument;
+        return null;
     }
 
-    private AqsResults getExistingAirQualityResultsByDate(String city, LocalDate date, List<AqsAirQualityDocument> aqsAirQualityDocumentList){
+    private AqsAirQualityDocument getExistingDocumentByCity(String city){
+        List<AqsAirQualityDocument> aqsAirQualityDocumentList = aqsAirQualityDocumentDao.findAll();
 
         for(AqsAirQualityDocument document : aqsAirQualityDocumentList){
             if(document.getCity().equals(city)){
-                for(AqsResults aqsResults : document.getResults()){
-                    if(aqsResults.getDate().equals(date)){
-                        return aqsResults;
-                    }
-                }
+                return document;
             }
         }
 
