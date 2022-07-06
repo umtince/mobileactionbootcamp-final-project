5448b0458e80fbfcb32ba8500e1eb6414a6fff3d
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2022-07-07 01:31:29 +0300

    consumer rabbit mq

--- log-service/src/main/java/com/mobileactionbootcamp/logservice/document/LogDocument.java
+++ log-service/src/main/java/com/mobileactionbootcamp/logservice/document/LogDocument.java
@@ -1,8 +1,12 @@
 package com.mobileactionbootcamp.logservice.document;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
 @Document("log")
 public class LogDocument {
     @Id
